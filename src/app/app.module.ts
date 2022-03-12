7e6ed7bbaece7336c92c0d405d2c3208b3eefd6d
Krishna Ananthi Thanupillai <krishna.thanupillai@siemens.com>
2022-03-12 21:53:50 +0530

    intial code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,28 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BoardComponent } from './board/board.component';
 import { BoxComponent } from './box/box.component';
-import { SharedModule } from './shared/shared.module';
-import { HttpClient } from '@angular/common/http';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
-
-export function TranslateLoaderFactory(http: HttpClient): TranslateHttpLoader {
-  return new TranslateHttpLoader(http, 'assets/i18n/', '.json');
-}
 
 @NgModule({
   declarations: [AppComponent, BoardComponent, BoxComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    SharedModule,
-    TranslateModule.forRoot({
-      loader: {
-        provide: TranslateLoader,
-        useFactory: TranslateLoaderFactory,
-        deps: [HttpClient],
-      },
-    }),
   ],
   providers: [],
   bootstrap: [AppComponent],
