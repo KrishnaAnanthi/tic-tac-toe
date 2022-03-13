d148a3c3aeac739fc2c95522f98b9d51b75c8051
Krishna Ananthi Thanupillai <krishna.thanupillai@siemens.com>
2022-03-13 18:49:52 +0530

    added test cases

--- src/app/board/board.component.spec.ts
+++ src/app/board/board.component.spec.ts
@@ -8,9 +8,8 @@ describe('BoardComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ BoardComponent ]
-    })
-    .compileComponents();
+      declarations: [BoardComponent],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
@@ -21,5 +20,44 @@ describe('BoardComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call newGame()', () => {
+    const spy = spyOn(component, 'newGame').and.returnValue(undefined);
+    expect(component.newGame()).toBeUndefined();
+    expect(component.newGame).toHaveBeenCalled();
+    expect(component.newGame).toHaveBeenCalledTimes(1);
+  });
+
+  it('after newGame() function call, boxes should be empty ', () => {
+    component.newGame();
+    component.boxes.every((element) => {
+      expect(element).toBeNull();
+    });
+  });
+
+  it('calculateWinner() should be called', () => {
+    const spy = spyOn(component, 'calculateWinner').and.returnValue(undefined);
+    expect(component.calculateWinner()).toBeUndefined();
+    expect(component.calculateWinner).toHaveBeenCalled();
+    expect(component.calculateWinner).toHaveBeenCalledTimes(1);
+  });
+
+  it('calculateWinner() - when player X win the game', () => {
+    component.boxes = ['X', 'X', 'X', 'O', 'O', '', '', '', ''];
+    const winner = component.calculateWinner();
+    expect(winner).toBe('X');
+  });
+
+  it('calculateWinner() - when player O win the game', () => {
+    component.boxes = ['X', 'X', 'O', 'O', 'O', 'O', 'X', 'X', ''];
+    const winner = component.calculateWinner();
+    expect(winner).toBe('O');
+  });
+
+  it('calculateWinner() - when the game is draw', () => {
+    component.boxes = ['X', 'X', 'O', 'O', 'X', 'X', 'X', 'O', 'O'];
+    const winner = component.calculateWinner();
+    expect(winner).toBe(null);
   });
 });
